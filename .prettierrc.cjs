51b15f3e58c3c4503ae8ee529d933282ba4389ee
william wang <549708589@qq.com>
2023-06-18 23:20:28 +0800

    feature: init

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  // 字符串是否使用单引号，默认为false，使用双引号
+  singleQuote: true,
+  // 行尾是否使用分号，默认为true
+  semi: false,
+  // 是否使用尾逗号，有三个可选值"<none|es5|all>"
+  trailingComma: 'none',
+  // 开启 eslint 支持
+  eslintIntegration: true,
+  endOfLine: 'auto'
+}
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-config-provider :locale="i18nLocale">
+    <router-view />
+  </el-config-provider>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+import { computed } from 'vue'
+import { GlobalStore } from '@/store'
+
+const globalStore = GlobalStore()
+
+// element 语言配置
+const i18nLocale = computed(() => {
+  if (globalStore.language && globalStore.language === 'zh') return zhCn
+  if (globalStore.language === 'en') return en
+  return zhCn
+})
+</script>

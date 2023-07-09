ce513cddcfa6dac61b006de5e1a60c8dac984fdb
william wang <549708589@qq.com>
2023-07-09 21:52:16 +0800

    feature: 动态表单组件

---
+++ src/components/FormRender/FormWidget/DateRangeWidget.vue
@@ -0,0 +1,55 @@
+<template>
+  <form-item-wrapper
+    :field="field"
+    :rules="rules"
+  >
+    <div :class="[customClass]">
+      <el-date-picker
+        ref="fieldEditor"
+        v-model="fieldModel"
+        :type="field.options.type"
+        :disabled="field.options.disabled"
+        :readonly="field.options.readonly"
+        :size="widgetSize"
+        :clearable="field.options.clearable"
+        :editable="field.options.editable"
+        :format="field.options.format"
+        :value-format="field.options.valueFormat"
+        :start-placeholder="field.options.startPlaceholder"
+        :end-placeholder="field.options.endPlaceholder"
+        @focus="handleFocusCustomEvent"
+        @blur="handleBlurCustomEvent"
+        @change="handleChangeEvent"
+      >
+      </el-date-picker>
+    </div>
+  </form-item-wrapper>
+</template>
+
+<script setup>
+import { defineComponent, getCurrentInstance, ref } from 'vue'
+import { commonProps, useCommonComputed, useEventFunction } from '@components/FormRender/FormWidget/common.js'
+import FormItemWrapper from '@components/FormRender/FormWidget/FormItemWrapper.vue'
+
+defineComponent({
+  name: 'DateRangeWidget'
+})
+
+const props = defineProps({
+  ...commonProps
+})
+
+const fieldEditor = ref()
+const oldFieldValue = ref(null)
+const fieldModel = ref(null)
+const rules = ref([])
+
+const { widgetSize, customClass } = useCommonComputed(props)
+const { handleBlurCustomEvent, handleFocusCustomEvent, handleChangeEvent } = useEventFunction(
+  getCurrentInstance,
+  props,
+  oldFieldValue
+)
+</script>
+
+<style scoped></style>
